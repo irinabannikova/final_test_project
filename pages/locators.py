0fea07e7c3af7a25799f66f350777cdc45810d63
Irina Bannikova <bannikova.irina5@wb.ru>
2024-12-10 12:24:48 +0300

    добавление тестов на проверку перехода на страницу логина

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,6 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-
 class LoginPageLocators():
      LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
      REGISTR_FORM = (By.CSS_SELECTOR, "#register_form")
@@ -16,4 +12,8 @@ class CatalogPageLocators():
      TEXT_ADD_TO_BASKET = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div > strong')
      MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
      PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
