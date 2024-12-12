803f76d421cb685a4ffac54f2f1a3c81fdedd5e1
Irina Bannikova <bannikova.irina5@wb.ru>
2024-12-12 13:48:14 +0300

    рефакторинг

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -16,6 +16,7 @@ from pages.basket_page import *
                                    pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, link):
     # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
     page = ProductPage(browser, link)
@@ -31,7 +32,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -39,7 +40,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
-
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "https://selenium1py.pythonanywhere.com/ru/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -86,6 +87,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         basket_page = BasketPage(browser, browser.current_url)
         basket_page.is_not_message_present()
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self,browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link)
